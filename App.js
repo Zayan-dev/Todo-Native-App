f6dfc0e32b21d6d78f3c2e372678afb1205085be
Zayan-dev <fa21bscs0095@maju.edu.pk>
2025-01-05 16:07:50 +0500

    accessCam code pushed

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import ListScreen from './screens/ListScreen';
+import AccessCam from './screens/AccessCam';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ListScreen />
+      {/* <ListScreen /> */}
+      <AccessCam></AccessCam>
     </View>
   );
 }
